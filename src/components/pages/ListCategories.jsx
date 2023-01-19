258559b96288d875fc53457feb67d148061faefa
Allassane <dickoallassane1997@gmail.com>
2023-01-19 15:37:11 +0100

    Add ListCategories components

--- src/components/pages/ListCategories.jsx
+++ src/components/pages/ListCategories.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 function ListCategories() {
   return (
     <main className='flex-center list-categories'>
+      <button type='button' className='btns-blue'>Check Status</button>
     </main>
   )
 }
