3b93f6141a06b05c7100cd99128a44909618886d
Allassane <dickoallassane1997@gmail.com>
2023-01-23 13:55:05 +0100

    Add configureStore.js to combine different store

--- src/index.js
+++ src/index.js
@@ -2,10 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux'
+import { configureStore } from '@reduxjs/toolkit'
+
+import rootReducer from './reducers/configureStore'
+
+const store = configureStore({ configureStore })
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
 );
---
+++ src/redux/configureStore.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux'
+
+const combineReducers = combineReducers({
+  booksReducer,
+  categoriesReducer
+})
