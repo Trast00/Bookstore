724b7bb0bd576e92ba0ecdd29510905ba746cba7
Allassane <dickoallassane1997@gmail.com>
2023-01-24 15:46:14 +0100

    Add createBookApi to create apiID

--- src/redux/books/bookAPI.js
+++ src/redux/books/bookAPI.js
@@ -1,8 +1,10 @@
 import { createAsyncThunk } from '@reduxjs/toolkit'
+import { v4 as uuidv4 } from 'uuid';
 
 
-const apiID = "AqeQv4g4DwJVH5Wl0GME"
-const apiUrl = `https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/${apiID}/books`
+
+const apiID = "dEKTcaMjVTpVo4TTyLFG"
+const apiUrl = `https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/${apiID}/books`
 
 export const fetchBooks = async () => {
   const result = await fetch(apiUrl)
@@ -12,3 +14,16 @@ export const fetchBooks = async () => {
   return data
 }
 
+const createBookApi = async () => {
+  const result = await fetch(
+    'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/',
+    {
+      method: 'POST',
+      headers: {"Content-Type": "application/json"}
+    }
+  )
+  console.log(result)
+  await result.json().then(id => {
+    console.log(id)
+  })
+}
